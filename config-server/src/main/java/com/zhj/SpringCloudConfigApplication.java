c9993ed11e5564a8c26e5688f326d4095195bb7d
1424132610@qq.com <1424132610@qq.com>
2019-09-24 15:56:34 +0800

    test

--- config-server/src/main/java/com/zhj/SpringCloudConfigApplication.java
+++ config-server/src/main/java/com/zhj/SpringCloudConfigApplication.java
@@ -1,35 +1,21 @@
 package com.zhj;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Hello world!
- *
  */
 @SpringBootApplication
 @EnableConfigServer
 @RestController
-public class SpringCloudConfigApplication
-{
-    @Value("${server.port:0}")
-    private Integer port;
+public class SpringCloudConfigApplication {
 
-    public static void main( String[] args )
-    {
+    public static void main(String[] args) {
         SpringApplication.run(SpringCloudConfigApplication.class);
     }
 
-
-
-    @RequestMapping("/info")
-    public Integer getInfo(){
-
-        return port;
-    }
 
 }
