b11ac183222142bed25bc6a301bfba220e28b290
1424132610@qq.com <1424132610@qq.com>
2019-09-24 10:59:28 +0800

    test

---
+++ config-server/src/main/java/com/zhj/SpringCloudConfigApplication.java
@@ -0,0 +1,33 @@
+package com.zhj;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+@EnableConfigServer
+@RestController
+public class SpringCloudConfigApplication
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(SpringCloudConfigApplication.class);
+    }
+
+
+
+    @RequestMapping("/info")
+    public String getInfo(){
+
+        return "";
+    }
+
+}
