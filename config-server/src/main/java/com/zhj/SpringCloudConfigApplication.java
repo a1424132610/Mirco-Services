bf7904b7632175bc7fb2b39445a9000c0702afd7
1424132610@qq.com <1424132610@qq.com>
2019-09-24 14:31:12 +0800

    test

--- config-server/src/main/java/com/zhj/SpringCloudConfigApplication.java
+++ config-server/src/main/java/com/zhj/SpringCloudConfigApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
